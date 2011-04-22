cc1b00bd2d7b73abc00baa0a1c60831cc2c75eef
James Reeves <jreeves@weavejester.com>
2011-04-22 21:58:48 +0100

    Improved 'Send Coins' dialog

--- src/send_coins.py
+++ src/send_coins.py
@@ -7,9 +7,19 @@ class SendCoinsDialog(object):
     def __init__(self):
         self.setup_ui()
         self.window = self.builder.get_object("window")
+        self.pay_to = self.builder.get_object("pay_to")
+        self.amount = self.builder.get_object("amount")
 
     def show(self):
         self.window.show()
+
+    def hide(self):
+        self.window.hide()
+        self.clear()
+
+    def clear(self):
+        self.pay_to.set_text("")
+        self.amount.set_text("")
 
     def setup_ui(self):
         current_dir = os.path.dirname(__file__)
@@ -19,11 +29,18 @@ class SendCoinsDialog(object):
         self.builder.connect_signals(self)
 
     def on_window_delete_event(self, window, data=None):
-        self.window.hide()
+        self.hide()
         return True
 
     def on_cancel_pressed(self, button, data=None):
-        self.window.hide()
+        self.hide()
 
     def on_send_pressed(self, button, data=None):
         pass
+
+    def on_paste_pressed(self, button, data=None):
+        clipboard = gtk.clipboard_get()
+        clipboard.request_text(self.paste_pay_to)
+
+    def paste_pay_to(self, _, address, data=None):
+        self.pay_to.set_text(address)
