225d9a9e5e76f0aa0be4ebc8ff4c1e9064457850
James Reeves <jreeves@weavejester.com>
2011-04-23 12:48:20 +0100

    Balance dialog now shows correct balance

--- src/balance.py
+++ src/balance.py
@@ -9,9 +9,24 @@ class BalanceDialog(window.Base):
         window.Base.__init__(self, "balance.xml")
         self.client = client
         self.window = self.builder.get_object("window")
+        self.balance = self.builder.get_object("balance")
+        self.client.on_transaction(self.update_balance)
+
+    def update_balance(self):
+        "Update the balance label to show the current balance."
+        if self.is_visible():
+            balance = self.client.get_balance()
+            label_text = "<b>%.2f BTC</b>" % balance
+            self.balance.set_label(label_text)
+
+    def is_visible(self):
+        "Returns true if the dialog is visible to the user."
+        return self.window.get_property("visible")
 
     def show(self):
+        "Show the dialog box to the user."
         self.window.show()
+        self.update_balance()
         
     def on_window_close(self, window, data=None):
         self.window.hide()
